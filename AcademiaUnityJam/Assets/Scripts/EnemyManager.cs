0c8da1da424c1cb0dda4fce2f32be85dda407dc3
Gabriel Mendes Tury Guimarães <80724636+GabrielTury@users.noreply.github.com>
2023-03-09 12:41:21 -0300

    Melee enemy
    Animaotr + animations of melee enemy

--- AcademiaUnityJam/Assets/Scripts/EnemyManager.cs
+++ AcademiaUnityJam/Assets/Scripts/EnemyManager.cs
@@ -5,15 +5,47 @@ using UnityEngine;
 public class EnemyManager : MonoBehaviour
 {
 
-    // Start is called before the first frame update
-    void Start()
+    public void CreateAura(int auraColor, int manyAuras, BasicEnemy enemy)
+
     {
-        
+        enemy.auraShield1 = auraColor;
+
+        if (manyAuras >= 1)
+        {
+            do
+            {
+
+                enemy.auraShield2 = Random.Range(0, 3);
+
+            }
+            while (enemy.auraShield2 == enemy.auraShield1);
+
+            enemy.auraShield3 = -1;
+
+        }
+
+        if (manyAuras == 2)
+        {
+            do
+            {
+                enemy.auraShield3 = Random.Range(0, 3);
+
+            }
+            while (enemy.auraShield3 == enemy.auraShield1 || enemy.auraShield3 == enemy.auraShield1);
+        }
+        else if (manyAuras == 0)
+        {
+
+            enemy.auraShield2 = -1;
+            enemy.auraShield3 = -1;
+
+        }
+
+
+
+        //print(auraShield1 + auraShield2 + auraShield3); 
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+
+
 }
